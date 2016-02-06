63445f1fa5f25de716f1fd40a9b44bf5a3b59508
jcarver <jcarver@linkedin.com>
2016-02-06 14:13:37 -0800

    start to add syntax highlighting and documentation

---
+++ examples/code.js
@@ -0,0 +1,17 @@
+// Extract the source code from a tag and place it in a <pre><code>source</code></pre> tag
+function codify(selector, targetSelector, preClass) {
+  var element = document.querySelector(selector)
+  var target = document.querySelector(targetSelector)
+  var html = element.innerHTML
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+
+  var code = document.createElement("code")
+  code.innerHTML = html
+
+  var pre = document.createElement("pre")
+  pre.classList.add(preClass)
+  pre.appendChild(code)
+  target.appendChild(pre)
+}
