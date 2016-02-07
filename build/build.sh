2320d259741da69f4a9ffda38dad374a274e6630
jcarver <jcarver@linkedin.com>
2016-02-06 17:21:05 -0800

    mv compiler.jar to build/

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 coffee --bare --compile --output target/coffeescript/ src/coffeescript/
-java -jar compiler.jar  --js target/coffeescript/**.js --js_output_file dist/suave-charts.min.js
+java -jar build/compiler.jar  --js target/coffeescript/**.js --js_output_file dist/suave-charts.min.js
